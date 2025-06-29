1facee576046e45fd2fc7619c5afd4c6828defa6
gayoung <gayoung990911@gmail.com>
2025-06-30 00:50:11 +0900

    Initial Commit

---
+++ worgleworgle_ios/worgleworgle_iosTests/worgleworgle_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  worgleworgle_iosTests.swift
+//  worgleworgle_iosTests
+//
+//  Created by 신가영 on 6/29/25.
+//
+
+import Testing
+@testable import worgleworgle_ios
+
+struct worgleworgle_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
